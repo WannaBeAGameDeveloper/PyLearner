53a0a8c6f1c59cb6353a794679b676923413f88c
WannaBeAGameDeveloper <55683964+WannaBeAGameDeveloper@users.noreply.github.com>
2019-09-25 16:32:02 +0800

    变量的算术运算
    在输出时使用了字符串的format方法进行格式化输出

---
+++ My-100-Days/Day002/GCode002-01.py
@@ -0,0 +1,18 @@
+'''
+使用变量保存数据并进行算术运算
+
+version:0.1
+'''
+
+a = 321
+b = 123
+
+#对示例的代码进行了修改
+print("a = {}, b = {}".format(a, b))
+print("a + b = {}".format(a + b))
+print("a - b = {}".format(a - b))
+print("a * b = {}".format(a * b))
+print("a / b = {}".format(a / b))
+print("a // b = {}".format(a // b))
+print("a % b = {}".format(a % b))
+print("a ** b = {}".format(a ** b))
